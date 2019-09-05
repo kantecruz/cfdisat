da143833f9872661fd9d4e0e88b430a04744475c
ckante <carloskante@hotmail.com>
2019-09-05 15:19:33 -0500

    ADD PROJECT

---
+++ Sat/Sat/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Sat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folio = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.re = new System.Windows.Forms.TextBox();
+            this.rr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.monto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // folio
+            // 
+            this.folio.Location = new System.Drawing.Point(123, 58);
+            this.folio.Name = "folio";
+            this.folio.Size = new System.Drawing.Size(354, 20);
+            this.folio.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 147);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(439, 83);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // re
+            // 
+            this.re.Location = new System.Drawing.Point(123, 29);
+            this.re.Name = "re";
+            this.re.Size = new System.Drawing.Size(100, 20);
+            this.re.TabIndex = 3;
+            // 
+            // rr
+            // 
+            this.rr.Location = new System.Drawing.Point(352, 32);
+            this.rr.Name = "rr";
+            this.rr.Size = new System.Drawing.Size(100, 20);
+            this.rr.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "RFC EMISOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "RFC RECEPTOR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "UUID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "MONTO";
+            // 
+            // monto
+            // 
+            this.monto.Location = new System.Drawing.Point(123, 93);
+            this.monto.Name = "monto";
+            this.monto.Size = new System.Drawing.Size(111, 20);
+            this.monto.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 306);
+            this.Controls.Add(this.monto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rr);
+            this.Controls.Add(this.re);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.folio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox folio;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox re;
+        private System.Windows.Forms.TextBox rr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox monto;
+    }
+}
+
---
+++ Sat/Sat/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sat.SatService;
+namespace Sat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string datos = "";
+            try
+            {
+                
+                using (SatService.ConsultaCFDIServiceClient
+                    client = new SatService.ConsultaCFDIServiceClient("BasicHttpBinding_IConsultaCFDIService"))
+                {
+                    textBox2.Text += "Calling WebService...";
+                    datos = "?re="+re.Text+"&rr="+rr.Text+"&tt="+monto.Text+"&id="+folio.Text;
+                    SatService.Acuse acuse = client.Consulta(datos);
+                    if (acuse == null)
+                    {
+                        textBox2.Text += "ERROR:: Acuse is null";
+
+                    }
+                    else
+                    {
+                        textBox2.Text += "\r\nEstado:: " + acuse.Estado + "\r\nCodigoEstatus::" + acuse.CodigoEstatus;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox2.Text += "Exception:: " + ex.Message;
+                MessageBox.Show("ERROR::" + ex.Message);
+            }
+        }
+    }
+}
